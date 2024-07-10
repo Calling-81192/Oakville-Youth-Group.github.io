afd7d62e8f9a16e6ab68bf3af8c61b5f7e3a546a
Calling-81192 <130265022+Calling-81192@users.noreply.github.com>
2024-07-09 20:30:30 -0400

    Add files via upload

--- maincontainer1.js
+++ maincontainer1.js
@@ -1,15 +1,41 @@
-// Get the container element
-var container = document.getElementById("myContainer");
+window.addEventListener('scroll', function() {
+  var fundraiser = document.getElementById('fundraiser');
+  var news=document.getElementById('news');
+  var current_event=document.getElementById('current_event');
+  var purchase=document.getElementById('purchase');
+  var goal=document.getElementById('goal');
 
-// Add the scroll event listener to the container
-container.addEventListener("scroll", function() {
-  // Get the absolute scroll position
-  var scrollTop = container.scrollTop;
 
-  // Change the background image based on the scroll position
-  if (scrollTop < 400) {
-    container.style.backgroundImage = "url('image1.jpg')";
+
+  var scrollPosition = window.scrollY;
+
+  if (scrollPosition > 248) { // Change '100' to the scroll position you want
+      fundraiser.style.opacity = '1'; // This will make the content inside the div invisible
   } else {
-    container.style.backgroundImage = "url('image2.jpg')";
+      fundraiser.style.opacity = '0'; // This will make the content inside the div visible
+  }
+
+  if (scrollPosition>992){
+      news.style.opacity = '1';
+  } else{
+      news.style.opacity='0';
+  }
+
+  if (scrollPosition>1353){
+      current_event.style.opacity = '1';
+  } else{
+      current_event.style.opacity='0';
+  }
+    
+  if (scrollPosition>1789){
+    purchase.style.opacity = '1';
+  } else{
+    purchase.style.opacity='0';
+  }
+
+  if (scrollPosition>2182){
+    goal.style.opacity = '1';
+  } else{
+    goal.style.opacity='0';
   }
 });
